f3efeec77f0e7b6e601019540b40710ec352fe8b
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-01-18 19:12:24 -0400

    rest e spread

--- src/rest-spread.js
+++ src/rest-spread.js
@@ -1,3 +1,5 @@
+// funcionamento do operador Arguments
+
 function somar() {
   let soma = 0;
 
@@ -6,7 +8,59 @@ function somar() {
   }
 
   return soma;
-  // console.log(arguments);
 }
 
 console.log(somar(10, 20, 23, 54, 65, 76, 2));
+
+// funcionamento do operador Rest
+
+// reticencias singnifica que pode ser recebido inumeros argumentos
+function somarComRest(...numeros) {
+  const soma = numeros.reduce((total, numeroAtual) => {
+    total += numeroAtual;
+    return total;
+  }, 0);
+  return soma;
+}
+console.log(somarComRest(12, 23, 43, 54));
+
+// funcionamento do operador Spread
+
+const numeros = [1, 2, 3, 4];
+console.log(...numeros);
+
+const equipesF1Top = ["Ferrari", "Mercedes", "RedBull"];
+const equipesF1Mid = ["Aston Martin", "McLaren", "Alpine"];
+
+// const equipesF1 = equipesF1Top.concat(equipesF1Mid);
+const equipesF1 = [...equipesF1Top, ...equipesF1Mid];
+
+console.log(equipesF1);
+
+const carroDoArtur = {
+  marca: "ford",
+  modelo: "ka",
+  motor: "1.0",
+};
+
+const carroDoLucas = {
+  ...carroDoArtur,
+  motor: "1.5",
+};
+console.log(carroDoLucas);
+
+// desestruturação
+
+// const motorDoCarroLucas = carroDoLucas.motor;
+const { motor: motorDoCarroLucas } = carroDoLucas;
+const { motor: motorDoCarroArtur } = carroDoArtur;
+
+const [item1, item2, item3, ...outrasEquipesF1] = equipesF1;
+
+console.log(item1);
+console.log(item2);
+console.log(item3);
+console.log(outrasEquipesF1);
+
+// spread = copiar/espalhar os dados
+// rest = restante do conteudo
